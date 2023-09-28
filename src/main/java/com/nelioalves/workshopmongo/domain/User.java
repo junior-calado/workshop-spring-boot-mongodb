207ad94eae992f1afe7ad54535bb44b5954a4cc5
junior-calado <junior_calado@hotmail.com>
2023-09-28 09:57:36 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,10 +1,17 @@
 package com.nelioalves.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Document(collection = "user")
 public class User implements Serializable {
 
+
+    @Id
     private String id;
     private String name;
     private String email;
@@ -12,9 +19,7 @@ public class User implements Serializable {
 
 
     public User() {
-
     }
-
 
     public User(String id, String name, String email) {
         this.id = id;
